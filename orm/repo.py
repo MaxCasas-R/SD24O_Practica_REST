5f7817073397b516cc8ee2c6a1a40d9b62ca6e0c
Max Casas <ezromax18@hotmail.com>
2024-12-04 19:21:37 -0600

    Repo.py terminado

--- orm/repo.py
+++ orm/repo.py
@@ -2,7 +2,6 @@ import orm.modelos as modelos
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 
-
 #--------------------Peticiones a alumnos
 #Select * from app.usuarios
 def devuelve_alumnos(sesion:Session):
@@ -10,9 +9,52 @@ def devuelve_alumnos(sesion:Session):
     return sesion.query(modelos.Alumno).all()
 
 #SELECT * FROM app.alumnos WHERE id={id_al}
-def alumno_por_id(sesion:Session, id_usuario:int):
+def alumno_por_id(sesion:Session, id_al:int):
     print("Devolviendo alumno por id")
-    return sesion.query(modelos.Alumno).filter(modelos.Alumno.id==id_usuario).firts()
+    return sesion.query(modelos.Alumno).filter(modelos.Alumno.id==id_al).firts()
+
+#-----DELETE------
+#DELETE FROM app.alumnos WHERE id_alumnos={id_al}
+def borrar_por_idAlumno(sesion:Session, id_al:int):
+    print("Borrando alumno por id_Alumno")
+    alm=alumno_por_id(sesion, id_al) #llamamos a la funcion alumno_por_id para comprobar que exista el usuario, para despues borrar
+    if alm is not None:
+        sesion.delete(alm) #pasamos la variable de la busqueda "Alm"
+        sesion.commit() #confirmamos cambios
+    respuesta={
+        "mensaje":"Usuario eliminado"
+    }
+    return respuesta
+
+#-----------------------Peticiones a calificaciones
+#select * from calificaciones 
+def devuelve_calificaciones(sesion:Session):
+    print("Devolviendo calificaciones")
+    return sesion.query(modelos.Calificacion).all()
+
+#SELECT * FROM app.calificaciones WHERE id={id_fo}
+def calificacion_por_id(sesion:Session, id_calificacion:int):
+    print("Devolviendo calificaciones por id")
+    return sesion.query(modelos.Calificacion).filter(modelos.Calificacion.id==id_calificacion).first()
+
+#SELECT * FROM app.calificaciones WHERE id_alumnos={id_al}
+def calificacion_por_idAlumno(sesion:Session, id_al:int):
+    print("Devolviendo calificacion por Id_Alumno")
+    return sesion.query(modelos.Calificacion).filter(modelos.Calificacion.id_alumno==id_al).first()
+
+#-----DELETE------
+#DELETE FROM app.calificaciones WHERE id_alumnos={id_al}
+def borrar_calificacion_por_idAlumno(sesion:Session, id_al:int):
+    print("Borrando calificacion por id del alumno")
+    alm=calificacion_por_idAlumno(sesion, id_al) #Llamamos a funcion
+    if alm is not None:
+        sesion.delete(alm)
+        sesion.commit()
+    respuesta={
+        "mensaje":"calificacion borrada por id_Alumno"
+    }
+    return respuesta
+
 
 #---------------------Peticiones a fotos
 #SELECT * FROM app.fotos
@@ -26,9 +68,20 @@ def foto_por_id(sesion:Session, id_foto):
     return sesion.query(modelos.Foto).filter(modelos.Foto.id==id_foto).first()
 
 #SELECT * FROM app.fotos WHERE id_alumnos={id_al}
-def foto_por_idAlumno(sesion:Session, id_alumno:int):
+def foto_por_idAlumno(sesion:Session, id_al:int):
     print("Devolviendo foto por id_alumno")
-    return sesion.query(modelos.Foto).filter(modelos.Foto.id_alumno==id_alumno).first()
+    return sesion.query(modelos.Foto).filter(modelos.Foto.id_alumno==id_al).first()
 
-#-----------------------Peticiones a calificaciones
-#Le dejamos por hoy aqui:)
+#-----DELETE------
+#DELETE FROM app.fotos WHERE id_alumnos={id_al}
+def borrar_foto_por_idAlumno(sesion:Session ,id_al:int):
+    print("Borrando foto por idAlumno")
+    alm=foto_por_idAlumno(sesion, id_al) #llamamos a la funcion
+    if alm is not None:
+        sesion.delete(id_al)
+        sesion.commit()
+    respuesta={
+        "mensaje":"Se ha eliminado la foto"
+    }
+    return respuesta
+
